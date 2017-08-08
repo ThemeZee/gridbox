405e4cc2486e044d042cee2886574255f5c3d71e
Thomas W <kontakt@themecoder.de>
2017-08-08 15:42:55 +0200

    added selective refresh to Sidebar Position setting

--- inc/customizer/sections/customizer-general.php
+++ inc/customizer/sections/customizer-general.php
@@ -18,96 +18,53 @@ function gridbox_customize_register_general_settings( $wp_customize ) {
 	$wp_customize->add_section( 'gridbox_section_general', array(
 		'title'    => esc_html__( 'General Settings', 'gridbox' ),
 		'priority' => 10,
-		'panel' => 'gridbox_options_panel',
-		)
-	);
+		'panel'    => 'gridbox_options_panel',
+	) );
 
-	// Add Settings and Controls for Layout.
+	// Add Settings and Controls for Sidebar Position.
 	$wp_customize->add_setting( 'gridbox_theme_options[layout]', array(
 		'default'           => 'right-sidebar',
-		'type'           	=> 'option',
-		'transport'         => 'refresh',
+		'type'              => 'option',
+		'transport'         => 'postMessage',
 		'sanitize_callback' => 'gridbox_sanitize_select',
-		)
-	);
+	) );
+
 	$wp_customize->add_control( 'gridbox_theme_options[layout]', array(
 		'label'    => esc_html__( 'Theme Layout', 'gridbox' ),
 		'section'  => 'gridbox_section_general',
 		'settings' => 'gridbox_theme_options[layout]',
 		'type'     => 'radio',
-		'priority' => 1,
+		'priority' => 10,
 		'choices'  => array(
-			'left-sidebar' => esc_html__( 'Left Sidebar', 'gridbox' ),
+			'left-sidebar'  => esc_html__( 'Left Sidebar', 'gridbox' ),
 			'right-sidebar' => esc_html__( 'Right Sidebar', 'gridbox' ),
-			),
-		)
-	);
+		),
+	) );
 
-	// Add Sticky Header Setting.
-	$wp_customize->add_setting( 'gridbox_theme_options[sticky_header_title]', array(
-		'default'           => '',
-		'type'           	=> 'option',
-		'transport'         => 'refresh',
-		'sanitize_callback' => 'esc_attr',
-		)
-	);
+	// Add Sticky Header title.
 	$wp_customize->add_control( new Gridbox_Customize_Header_Control(
 		$wp_customize, 'gridbox_theme_options[sticky_header_title]', array(
-		'label' => esc_html__( 'Sticky Header', 'gridbox' ),
-		'section' => 'gridbox_section_general',
-		'settings' => 'gridbox_theme_options[sticky_header_title]',
-		'priority' => 2,
+			'label'    => esc_html__( 'Sticky Header', 'gridbox' ),
+			'section'  => 'gridbox_section_general',
+			'settings' => array(),
+			'priority' => 20,
 		)
 	) );
+
+	// Add Setting and Control for sticky header.
 	$wp_customize->add_setting( 'gridbox_theme_options[sticky_header]', array(
 		'default'           => false,
-		'type'           	=> 'option',
+		'type'              => 'option',
 		'transport'         => 'refresh',
 		'sanitize_callback' => 'gridbox_sanitize_checkbox',
-		)
-	);
+	) );
+
 	$wp_customize->add_control( 'gridbox_theme_options[sticky_header]', array(
 		'label'    => esc_html__( 'Enable sticky header feature', 'gridbox' ),
 		'section'  => 'gridbox_section_general',
 		'settings' => 'gridbox_theme_options[sticky_header]',
 		'type'     => 'checkbox',
-		'priority' => 3,
-		)
-	);
-
-	// Add Homepage Title.
-	$wp_customize->add_setting( 'gridbox_theme_options[blog_title]', array(
-		'default'           => '',
-		'type'           	=> 'option',
-		'transport'         => 'refresh',
-		'sanitize_callback' => 'wp_kses_post',
-		)
-	);
-	$wp_customize->add_control( 'gridbox_theme_options[blog_title]', array(
-		'label'    => esc_html__( 'Blog Title', 'gridbox' ),
-		'section'  => 'gridbox_section_general',
-		'settings' => 'gridbox_theme_options[blog_title]',
-		'type'     => 'text',
-		'priority' => 4,
-		)
-	);
-
-	// Add Homepage Title.
-	$wp_customize->add_setting( 'gridbox_theme_options[blog_description]', array(
-		'default'           => '',
-		'type'           	=> 'option',
-		'transport'         => 'refresh',
-		'sanitize_callback' => 'wp_kses_post',
-		)
-	);
-	$wp_customize->add_control( 'gridbox_theme_options[blog_description]', array(
-		'label'    => esc_html__( 'Blog Description', 'gridbox' ),
-		'section'  => 'gridbox_section_general',
-		'settings' => 'gridbox_theme_options[blog_description]',
-		'type'     => 'textarea',
-		'priority' => 5,
-		)
-	);
-
+		'priority' => 30,
+	) );
 }
 add_action( 'customize_register', 'gridbox_customize_register_general_settings' );
