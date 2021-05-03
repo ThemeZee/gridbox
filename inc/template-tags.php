34afe953be70ab70fd777a8e8f86778d8535d2b6
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 18:08:02 +0200

    fixed typo in pagination function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -381,7 +381,7 @@ if ( ! function_exists( 'gridbox_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'gridbox' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'gridbox' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'gridbox' ) . '</span>&raquo;',
 		) );
 
